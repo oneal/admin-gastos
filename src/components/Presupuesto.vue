e070fb15655254616394f7330fa4d82bf98978cb
Oneal-Portatil <oneal152@gmail.com>
2024-03-11 23:46:51 +0100

    creando formulario y estilos

---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <form>
+    <div class="campo">
+      <label for="nuevo-presupuesto">Definir presupuesto</label>
+      <input
+          id="nuevo-presupuesto"
+          class="nuevo-presupuesto"
+          placeholder="Añadir nuevo presupuesto"
+      />
+    </div>
+    <input type="submit" value="Definir presupuesto" />
+  </form>
+</template>
+
+<style scoped>
+
+</style>
